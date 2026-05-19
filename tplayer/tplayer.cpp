41633fb8f1d4472df6d65f11221a736f45fa7a16
John Else <john.else@gmail.com>
2026-05-19 10:01:50 +0200

    Add argument parsing

--- tplayer/tplayer.cpp
+++ tplayer/tplayer.cpp
@@ -1,7 +1,35 @@
 #include <argparse/argparse.hpp>
 #include <miniaudio/miniaudio.h>
 
+#include <iostream>
+#include <stddef.h>
+
+static constexpr size_t DEFAULT_FRAMES = 2048;
+
 int main(int argc, char ** argv)
 {
+    argparse::ArgumentParser parser("tplayer");
+
+    parser.add_argument("--frames")
+        .help("The number of frames per waveform")
+        .default_value(DEFAULT_FRAMES)
+        .scan<'u', size_t>();
+
+    parser.add_argument("path")
+        .help("The path to the wavetable file to open");
+
+    try
+    {
+        parser.parse_args(argc, argv);
+    }
+    catch (const std::exception &err)
+    {
+        std::cerr << err.what() << std::endl;
+        return 1;
+    }
+
+    const std::string path = parser.get<std::string>("path");
+    const size_t frames = parser.get<size_t>("frames");
+
     return 0;
 }
