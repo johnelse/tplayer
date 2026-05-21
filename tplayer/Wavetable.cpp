94d93450e49fc7ff97751c7999135d8049975219
John Else <john.else@gmail.com>
2026-05-21 12:23:43 +0200

    Wavetable: Simplify by removing some intermediate variables

--- tplayer/Wavetable.cpp
+++ tplayer/Wavetable.cpp
@@ -5,13 +5,11 @@ Wavetable::Wavetable(const std::string &path, ma_uint64 framesPerWave)
     , currentWave(0)
     , valid(false)
 {
-    ma_result result = ma_decoder_init_file(path.data(), NULL, &decoder);
-    if (result != MA_SUCCESS)
+    if (ma_decoder_init_file(path.data(), NULL, &decoder) != MA_SUCCESS)
         return;
 
     ma_uint64 totalFrames;
-    result = ma_decoder_get_length_in_pcm_frames(&decoder, &totalFrames);
-    if (result != MA_SUCCESS)
+    if (ma_decoder_get_length_in_pcm_frames(&decoder, &totalFrames) != MA_SUCCESS)
         return;
 
     if (totalFrames % framesPerWave)
@@ -51,14 +49,12 @@ size_t Wavetable::read(void *out, ma_uint64 frames)
     const ma_uint64 waveEnd = waveStart + framesPerWave;
     ma_uint64 cursor, framesToRead, read = 0;
 
-    ma_result result = ma_decoder_get_cursor_in_pcm_frames(&decoder, &cursor);
-    if (result != MA_SUCCESS)
+    if (ma_decoder_get_cursor_in_pcm_frames(&decoder, &cursor) != MA_SUCCESS)
         return read;
 
     if (cursor >= waveEnd)
     {
-        result = ma_decoder_seek_to_pcm_frame(&decoder, waveStart);
-        if (result != MA_SUCCESS)
+        if (ma_decoder_seek_to_pcm_frame(&decoder, waveStart) != MA_SUCCESS)
             return read;
 
         cursor = waveStart;
